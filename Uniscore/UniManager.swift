25107f25814a13c7d3bf4f7dc6ef551ad64e3f8c
Hannah St George (s5102531) <s5102531@mac42941.student.bournemouth.ac.uk>
2018-11-29 16:34:35 +0000

    UniManager.swift file added

---
+++ Uniscore/UniManager.swift
@@ -0,0 +1,9 @@
+//
+//  UniManager.swift
+//  Uniscore
+//
+//  Created by Hannah St George (s5102531) on 29/11/2018.
+//  Copyright © 2018 Jake Steptoe (s5105535). All rights reserved.
+//
+
+import Foundation
