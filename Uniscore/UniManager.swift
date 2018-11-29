eccda0cb145e3b8100e21bb84e7faf3b947556da
Hannah St George (s5102531) <s5102531@mac42941.student.bournemouth.ac.uk>
2018-11-29 16:37:23 +0000

    import Foundation import CoreLocation  class UniManager {          var uni = [uni]()          init() {                  let uniCoordinate = CLLocationCoordinate2D(latitude: 50.715272, longitude: -1.875261)         let uni = self.uni(coordinate: uniCoordinate, title: "Uni")         uni.append(uni) } }

--- Uniscore/UniManager.swift
+++ Uniscore/UniManager.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+import CoreLocation
+
+class UniManager {
+    
+    var uni = [uni]()
+    
+    init() {
+        
+        let uniCoordinate = CLLocationCoordinate2D(latitude: 50.715272, longitude: -1.875261)
+        let uni = self.uni(coordinate: uniCoordinate, title: "Uni")
+        uni.append(uni)
+}
+}
