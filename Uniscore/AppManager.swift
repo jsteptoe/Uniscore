47feb97d44752e0588b2055758376001fb93abc2
Jake Steptoe <s5105535@bournemouth.ac.uk>
2018-11-30 20:11:14 +0000

    Added code for automatic login/sign up feature

--- Uniscore/AppManager.swift
+++ Uniscore/AppManager.swift
@@ -4,16 +4,27 @@ import Firebase
 
 class AppManager {
     
+  
+    
     static let shared = AppManager()
     
     let storyboard = UIStoryboard(name: "Main", bundle: nil)
-    var AppContainer = AppContainerViewController!
-    
-    
-    
-    
+    var AppContainer: AppContainerViewController!
     
     private init() {  }
+      var appContainer: AppContainerViewController!
+    func showApp() {
+        
+        var viewController: UIViewController
+        if.Auth.auth().currentUser == nil {
+            viewController = storyboard.instantiateViewController(withIdentifier: "LoginViewController")
+        } else {
+            viewController = storyboard.instantiateViewController(withIdentifier: "MainViewController")
+        }
+        appContainer.present(viewController, animated: true, completion: nil)
+       
+        
+    }
     
     
     
