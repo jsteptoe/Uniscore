7f96a7afe50431ae41552ed63d19726fa8c244c3
Jake Steptoe <s5105535@bournemouth.ac.uk>
2018-11-30 18:43:53 +0000

    Created new ViewController
    LoginViewController

--- Uniscore/Uni.swift
+++ Uniscore/Uni.swift
@@ -1,15 +1,9 @@
-//
-//  Uni.swift
-//  Uniscore
-//
-//  Created by Hannah St George (s5102531) on 29/11/2018.
-//  Copyright © 2018 Jake Steptoe (s5105535). All rights reserved.
-//
+
 
 import Foundation
 import MapKit
 
-class Treasure: NSObject, MKAnnotation {
+class Uni: NSObject, MKAnnotation {
     
     var coordinate: CLLocationCoordinate2D
     var title: String?
